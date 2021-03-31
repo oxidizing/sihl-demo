5a0aab2269b6c4ddbdd97384b602f0fcb9683817
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2021-03-31 10:01:39 +0200

    fix postCreate commands and add missing package

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,17 +9,21 @@ opam remote remove --all default
 opam remote add default https://opam.ocaml.org
 
 # Pins
-opam pin add -yn ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git
+# opam pin add -yn ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git
 
 # install opam packages
 # e.g. when developing with emax, add also: utop merlin ocamlformat
-opam install caqti-driver-postgresql ocamlformat ocaml-lsp-server sihl
+opam install caqti-driver-postgresql ocamlformat ocaml-lsp-server.1.4.0 sihl alcotest-lwt
 
 # install project dependancies
+# pin package
 opam pin add pizza.dev . --no-action
+# Query and install external dependencies
 opam depext pizza --yes --with-doc
-opam install . --deps-only --with-doc --with-test --locked --unlock-base
+# install dependencies
+OPAMSOLVERTIMEOUT=180 opam install . --deps-only --with-doc --with-test --locked --unlock-base
 opam install ocamlformat --skip-updates
+# upgrade dependencies
 opam upgrade --fixup
 
 # initialize project and update environmemnt
